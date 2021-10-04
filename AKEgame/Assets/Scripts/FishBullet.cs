a0df66625209cdbb509e1514e6e6d8e9d6de81fe
khelelelle <khaylap.elot@gmail.com>
2021-10-04 21:02:08 +0800

    shooting + wand

---
+++ AKEgame/Assets/Scripts/FishBullet.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FishBullet : MonoBehaviour
+{
+    public float speed;
+    
+    private void Update()
+    {
+        transform.Translate(transform.right * transform.localScale.x * speed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            return;
+
+        Destroy(gameObject);
+    }
+
+
+}
