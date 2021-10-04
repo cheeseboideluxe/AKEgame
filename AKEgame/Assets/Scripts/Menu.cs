f445aa85b22ee47a71c463a3c712a5722c2da1f1
khelelelle <khaylap.elot@gmail.com>
2021-10-04 12:37:47 +0800

    font + bunch of other stuff sa menu

--- AKEgame/Assets/Scripts/Menu.cs
+++ AKEgame/Assets/Scripts/Menu.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void OnClickStartButton()
     {
-        
+        StartCoroutine(StartButtonRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator StartButtonRoutine()
     {
-        
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("2 Opening Cutscene");
+    }
+
+    public void OnClickExitButton()
+    {
+        Application.Quit();
     }
 }
+
