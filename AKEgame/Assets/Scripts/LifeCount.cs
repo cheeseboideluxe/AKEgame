00d7104d0f95d729d698e111307b9c6fe3efb9fe
khelelelle <khaylap.elot@gmail.com>
2021-10-10 15:42:29 +0800

    reverted changes

--- AKEgame/Assets/Scripts/LifeCount.cs
+++ AKEgame/Assets/Scripts/LifeCount.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LifeCount : MonoBehaviour
+public class LifeCount: MonoBehaviour
 {
     public Image[] lives;
     public int livesRemaining;
